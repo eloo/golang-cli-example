67db1aac4e55efea399575846c27eab4ba143158
Joseph Weigl <joseph.weigl@gmail.com>
2018-08-06 23:05:35 +0200

    Add push back

---
+++ .travis/push.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+push() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://eloo:${GH_TOKEN}@github.com/eloo/golang-cli-example.git > /dev/null 2>&1
+  git push origin master --quiet
+}
+
+setup_git
+
+push
