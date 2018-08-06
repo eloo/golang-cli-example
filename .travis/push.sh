d962152fa91398b8af192b70a2450ada28b8e0da
Joseph Weigl <joseph.weigl@gmail.com>
2018-08-06 23:07:24 +0200

    Fix token

--- .travis/push.sh
+++ .travis/push.sh
@@ -10,7 +10,7 @@ push() {
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://eloo:${GH_TOKEN}@github.com/eloo/golang-cli-example.git > /dev/null 2>&1
+  git remote add origin https://eloo:${GITHUB_RELEASE_TOKEN}@github.com/eloo/golang-cli-example.git > /dev/null 2>&1
   git push origin master --quiet
 }
 
